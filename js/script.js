d7a4fbef73eb6566eb1049a03c6d8fd6b7682746
Andi <galynski@yahoo.com>
2020-05-03 01:46:35 +0200

    new

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-$('.mainblock__row').magnificPopup({
-  delegate: 'a',
-  type: 'image',
-  gallery: {
+$(document).ready(function() {
+  $('.mainblock__row').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
       enabled: true,
-     }
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    }
   });
+});
